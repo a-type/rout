c3911ddc433f7260ae878c96bf2f2c250658c414
Grant Forrest <gaforres@gmail.com>
2025-05-01 16:23:03 -0400

    add admin check lol

--- services/api/src/routers/admin/adminRouter.ts
+++ services/api/src/routers/admin/adminRouter.ts
@@ -1,8 +1,8 @@
 import { Hono } from 'hono';
 import { Env } from '../../config/ctx';
+import { adminMiddleware } from '../../middleware/admin';
 import { adminGameProductsRouter } from './adminGameProductsRouter';
 
-export const adminRouter = new Hono<Env>().route(
-  '/gameProducts',
-  adminGameProductsRouter,
-);
+export const adminRouter = new Hono<Env>()
+  .use(adminMiddleware)
+  .route('/gameProducts', adminGameProductsRouter);
