c9547cd85ef4ee64c0b4bf4dd783850d732440fe
Grant Forrest <gaforres@gmail.com>
2025-06-13 22:50:24 -0400

    trying to reproduce hearts bugs

---
+++ packages/game-ui/src/tokens/TokenPresence.tsx
@@ -0,0 +1,7 @@
+import { AnimatePresence } from 'motion/react';
+
+/**
+ * Wrap your token components with this to enable animations
+ * when they are added or removed from the DOM.
+ */
+export const TokenPresence = AnimatePresence;
--- packages/game-ui/src/tokens/index.ts
+++ packages/game-ui/src/tokens/index.ts
@@ -1,6 +1,7 @@
 export { useIsDragging } from './dnd/Draggable';
 export * from './Token';
 export * from './TokenHand';
+export * from './TokenPresence';
 export * from './TokenRoot';
 export * from './TokenSpace';
 export * from './types';
