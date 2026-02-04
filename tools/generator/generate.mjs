491deabba3b1e7c1b59a217c9cc5ac374bca1343
Grant Forrest <gaforres@gmail.com>
2026-02-03 23:31:00 -0500

    fix generate script

--- tools/generator/generate.mjs
+++ tools/generator/generate.mjs
@@ -98,7 +98,6 @@ if (template === 'games') {
   // add to packages/games as a dependency and
   // to the game map.
   await addGameToGamesPackage();
-  await addGameToRendererPackage();
   await addGameDevTask();
 }
 
@@ -156,34 +155,6 @@ async function addGameToGamesPackage() {
   );
 
   await fs.writeFile(gamesIndex, gamesIndexLines.join('\n'));
-}
-
-async function addGameToRendererPackage() {
-  const gameRendererPackage = path.resolve(
-    __dirname,
-    '../../packages/game-renderer',
-  );
-  const gameRendererPackageJson = path.resolve(
-    gameRendererPackage,
-    'package.json',
-  );
-
-  const gameRendererPackageJsonContent = await fs.readFile(
-    gameRendererPackageJson,
-    'utf-8',
-  );
-  const gameRendererPackageJsonParsed = JSON.parse(
-    gameRendererPackageJsonContent,
-  );
-
-  gameRendererPackageJsonParsed.dependencies[
-    `@long-game/game-${name}-renderer`
-  ] = 'workspace:*';
-
-  await fs.writeFile(
-    gameRendererPackageJson,
-    JSON.stringify(gameRendererPackageJsonParsed, null, 2),
-  );
 }
 
 async function addGameDevTask() {
