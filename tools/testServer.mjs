cd1fb44db75edfc9bd8aaf3ef3e664ae8f5a2298
Grant Forrest <gaforres@gmail.com>
2026-04-13 23:18:14 -0400

    try adding some more fake secrets

--- tools/testServer.mjs
+++ tools/testServer.mjs
@@ -1,5 +1,9 @@
 import { exec } from 'node:child_process';
 
-exec('pnpm --filter "@long-game/service-api" run dev');
+exec('pnpm --filter "@long-game/service-api" run dev', {
+  env: {
+    UI_ORIGIN: 'http://localhost:3100',
+  },
+});
 exec('pnpm --filter "@long-game/app" run preview');
 console.log('Test servers started. Press Ctrl+C to stop.');
