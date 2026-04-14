57d26a7bb5f85d37d08841be8f6241c482d56d3d
Grant Forrest <gaforres@gmail.com>
2026-04-13 22:02:04 -0400

    idk

--- playwright.config.ts
+++ playwright.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm core:dev:ci',
+    command: 'pnpm run core:dev:ci',
     url: 'http://localhost:3100',
     reuseExistingServer: !process.env.CI,
   },
