ec8b9bc83058e99704871737398e23e13a91daac
Grant Forrest <gaforres@gmail.com>
2026-04-14 17:33:19 -0400

    fix test server to run number guess

--- tools/testServer.mjs
+++ tools/testServer.mjs
@@ -1,9 +1,42 @@
-import { exec } from 'node:child_process';
+import { spawn } from 'node:child_process';
 
-exec('pnpm --filter "@long-game/service-api" run dev', {
-  env: {
-    UI_ORIGIN: 'http://localhost:3100',
+const api = spawn(
+  'pnpm',
+  ['--filter', '@long-game/service-api', 'run', 'dev'],
+  {
+    env: {
+      UI_ORIGIN: 'http://localhost:3100',
+      ...process.env,
+    },
+    stdio: 'inherit',
   },
+);
+const ui = spawn('pnpm', ['--filter', '@long-game/app', 'run', 'preview'], {
+  stdio: 'inherit',
 });
-exec('pnpm --filter "@long-game/app" run preview');
+const numberGuess = spawn(
+  'pnpm',
+  ['--filter', '@long-game/game-number-guess-v1', 'run', 'dev'],
+  {
+    stdio: 'inherit',
+  },
+);
 console.log('Test servers started. Press Ctrl+C to stop.');
+
+api.channel?.on('close', (code) => {
+  console.log(`API server exited with code ${code}`);
+});
+ui.channel?.on('close', (code) => {
+  console.log(`UI server exited with code ${code}`);
+});
+numberGuess.channel?.on('close', (code) => {
+  console.log(`Number Guess server exited with code ${code}`);
+});
+
+process.on('SIGINT', () => {
+  console.log('Stopping test servers...');
+  api.kill();
+  ui.kill();
+  numberGuess.kill();
+  process.exit();
+});
