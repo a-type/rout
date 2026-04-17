c33166c46414afa5f3ae261edf7153b697d49b52
Grant Forrest <gaforres@gmail.com>
2026-04-17 16:37:47 -0400

    update manifest path

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,7 +10,7 @@ export function App({}: AppProps) {
   return (
     <ErrorBoundary fallback={<div>Error</div>}>
       <SdkProvider value={publicSdk}>
-        <UiProvider>
+        <UiProvider manifestPath="/manifest.webmanifest">
           <UpdateBanner />
           <Pages />
         </UiProvider>
