31ee53cd686963cc959e0725262b00f830a131d2
Grant Forrest <gaforres@gmail.com>
2026-04-17 11:11:16 -0400

    fix: add cors to registry

---
+++ services/game-registry/src/cors.ts
@@ -0,0 +1,33 @@
+import { cors } from 'hono/cors';
+
+export function configuredCors() {
+  return cors({
+    origin: (origin, ctx) => {
+      if (origin === ctx.env.UI_ORIGIN) {
+        return origin;
+      }
+      // integration tests
+      if (origin === 'http://localhost:3333') {
+        return origin;
+      }
+      return null;
+    },
+    credentials: true,
+    allowHeaders: [
+      'Authorization',
+      'Content-Type',
+      'X-Request-Id',
+      'X-Csrf-Token',
+    ],
+    exposeHeaders: [
+      'Content-Type',
+      'Content-Length',
+      'X-Request-Id',
+      'Set-Cookie',
+      'X-Long-Game-Error',
+      'X-Long-Game-Message',
+      'X-Csrf-Token',
+      'X-Auth-Error',
+    ],
+  });
+}
--- services/game-registry/src/index.ts
+++ services/game-registry/src/index.ts
@@ -1,30 +1,33 @@
 import { normalizeVersion } from '@long-game/common';
 import { Hono } from 'hono';
+import { configuredCors } from './cors';
 
 export default new Hono<{
   Bindings: GameRegistryBindings;
-}>().all('/:gameId/:version/*', async (c) => {
-  const { gameId, version } = c.req.param();
+}>()
+  .use(configuredCors())
+  .all('/:gameId/:version/*', async (c) => {
+    const { gameId, version } = c.req.param();
 
-  // normalize version just in case
-  const normalizedVersion = normalizeVersion(version);
+    // normalize version just in case
+    const normalizedVersion = normalizeVersion(version);
 
-  const bindingKey = `game--${gameId}--${normalizedVersion}`;
+    const bindingKey = `game--${gameId}--${normalizedVersion}`;
 
-  const binding = c.env[bindingKey as keyof GameRegistryBindings] as Fetcher;
-  if (!binding) {
-    return c.text(`Game not found: ${gameId} ${normalizedVersion}`, 404);
-  }
+    const binding = c.env[bindingKey as keyof GameRegistryBindings] as Fetcher;
+    if (!binding) {
+      return c.text(`Game not found: ${gameId} ${normalizedVersion}`, 404);
+    }
 
-  // forward to the appropriate game worker API
-  const forwardedPath = c.req.path.replace(`/${gameId}/${version}`, '');
-  // url origin is arbitrary.
-  return binding.fetch(`http://localhost:8080${forwardedPath}`, {
-    method: c.req.method,
-    headers: {
-      ...c.req.header(),
-      'X-Rout-Forwarded': 'true',
-    },
-    body: c.req.raw.body,
+    // forward to the appropriate game worker API
+    const forwardedPath = c.req.path.replace(`/${gameId}/${version}`, '');
+    // url origin is arbitrary.
+    return binding.fetch(`http://localhost:8080${forwardedPath}`, {
+      method: c.req.method,
+      headers: {
+        ...c.req.header(),
+        'X-Rout-Forwarded': 'true',
+      },
+      body: c.req.raw.body,
+    });
   });
-});
