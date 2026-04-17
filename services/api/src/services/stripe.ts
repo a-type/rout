a1a3459fa8a30be644c79185738f5421af891efc
Grant Forrest <gaforres@gmail.com>
2026-04-17 15:21:38 -0400

    update stripe lib

--- services/api/src/services/stripe.ts
+++ services/api/src/services/stripe.ts
@@ -4,7 +4,7 @@ let stripe: Stripe | undefined = undefined;
 export const getStripe = (env: ApiBindings) => {
   if (stripe) return stripe;
   stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: '2025-03-31.basil',
+    apiVersion: '2026-03-25.dahlia',
   });
   return stripe!;
 };
